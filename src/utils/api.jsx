18b936c468f4c39bdc080d67bb4c8741118365e4
Jason Tam <jzntam@gmail.com>
2016-04-03 00:55:38 -0700

    Refactor to use a Reflux store to fetch and store data

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -2,9 +2,8 @@ var Fetch   = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey  = require('./api_key.jsx') || process.env.IMGUR_API_KEY;
 
-module.exports = window.api = {
+module.exports = {
   get: function(url) {
-    console.log(apiKey['imgur']);
     return fetch(rootUrl + url, {
       headers: {
         'Authorization': 'Client-ID ' + apiKey
@@ -12,9 +11,6 @@ module.exports = window.api = {
     })
     .then(function(response) {
       return response.json()
-    })
-    .then(function(data) {
-      console.log(data)
     })
   }
 };
