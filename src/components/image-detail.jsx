7db6d8570c769d232e79c29a6f52443a63391db5
Jason Tam <jzntam@gmail.com>
2016-04-09 21:46:03 -0700

    add the image detail view

---
+++ src/components/image-detail.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function() {
+    return <div>
+      Hello World form the image-detail component. YEEHAW.
+    </div>
+  }
+});
