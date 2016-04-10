1b9f9d0619111afcfe5719b6d6436c083673f54e
Jason Tam <jzntam@gmail.com>
2016-04-09 21:49:37 -0700

    Rendering api data for single image on screen for image detail

--- src/components/image-detail.jsx
+++ src/components/image-detail.jsx
@@ -1,9 +1,28 @@
-var React = require('react');
+var React      = require('react');
+var Reflux     = require('reflux');
+var ImageStore = require('../stores/image-store');
+var Actions    = require('../actions');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      image: {}
+    }
+  },
+  componentWillMount: function() {
+    Actions.getImage(this.props.params.id);
+  },
   render: function() {
     return <div>
-      Hello World form the image-detail component. YEEHAW.
+      { this.state.image.title }
     </div>
+  },
+  onChange: function() {
+    this.setState({
+      image: ImageStore.find(this.props.params.id)
+    });
   }
 });
